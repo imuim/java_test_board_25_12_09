eb1e55e476a9c88ca44cd0391ef4badf4dae095a
imuim <imjaeu0898@gmail.com>
2025-12-10 07:21:08 +0900

    요구사항1 완료

--- src/main/java/com/ljw/Main.java
+++ src/main/java/com/ljw/Main.java
@@ -1,9 +1,24 @@
 package com.ljw;
 
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+
+
+        while(true) {
+                System.out.println("==자바 게시판 시작==");
+                String cmd = scanner.nextLine();
+                if(cmd.equals("exit")) {
+                System.out.println("명령"+cmd);
+                System.out.println("==자바 게시판 종료==");
+                break;
+
+                }
+            }
         }
     }
